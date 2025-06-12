8b226e2a9f84128a49d08ad0a01a4ad6f99c8e7d
MaxSNava <max.s015@outlook.com>
2025-06-11 22:54:38 -0600

    Creacion de objetos

--- IntroCsharpBackend/Program.cs
+++ IntroCsharpBackend/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+var sale = new Sale(5);
+var message = sale.GetInfo();
+
+Console.WriteLine(message);
+
+public class Sale
+{
+    // Las propiedades se escriben en PascalCase 
+    public decimal Total { get; set; }
+
+    public Sale(decimal total)
+    {
+        if (total < 0)
+            throw new ArgumentException("El total no puede ser negativo.", nameof(total));
+
+        Total = total;
+    }
+
+    public string GetInfo()
+    {
+        // Las variables locales se escriben en camelCase
+        var info = $"Total de la venta: {Total}";
+        return info;
+    }
+}
